3a9bc38fc75ad3aeaf3de760f450b955400f09f6
JenPiwonskiGA <jenpiwonski@gmail.com>
2020-02-06 22:32:47 -0500

    improved nutritional info css

--- script.js
+++ script.js
@@ -1,15 +1,7 @@
-// {
-//   "appId":"YOUR_APP_ID",
-//   "appKey":"YOU_APP_KEY",
-//   "Phrase":"search_item"
-// }
-
 const DOMAIN = `https://api.nutritionix.com/`;
 const API_KEY = `b886b748dc5ea57054a126ca75eb1bf9`;
 const APP_ID = `264e5938`;
 const sectionOutput = document.querySelector("#nutritionLabel");
-// const PHRASE = "";
-// const BASE_URL = `${DOMAIN}v1_1/search/${input.value}&appId=${APP_ID}&appKey=${API_KEY}`;
 
 let button = document.querySelector(`button`);
 let input = document.querySelector(`input`);
@@ -28,30 +20,30 @@ let renderList = field => {
     let servingSize = fields.fields.nf_serving_size_qty;
     let servingSizeUnit = fields.fields.nf_serving_size_unit;
     let calories = fields.fields.nf_calories;
-    let sugar = fields.fields.nf_sugars;
-    let sodium = fields.fields.nf_sodium;
     let totalFat = fields.fields.nf_total_fat;
     let saturatedFat = fields.fields.nf_saturated_fat;
     let transFat = fields.fields.nf_trans_fatty_acid;
     let cholesterol = fields.fields.nf_cholesterol;
+    let sodium = fields.fields.nf_sodium;
     let totalCarbs = fields.fields.nf_total_carbohydrate;
     let fiber = fields.fields.nf_dietary_fiber;
+    let sugar = fields.fields.nf_sugars;
     let protein = fields.fields.nf_protein;
 
     let newDiv = document.createElement("div");
     newDiv.classList.add("nutritionInfo");
-    newDiv.innerHTML += `<h2>Item: ${itemName}</h2>
+    newDiv.innerHTML += `<h2 id="topItem">${itemName}</h2>
         <h2>Brand: ${brandName}</h2>
         <h3>${servingSize} ${servingSizeUnit}</h3>
         <h3>Calories: ${calories}</h3>
         <h3>Total Fat: ${totalFat}g</h3>
-        <h4>Saturated Fat: ${saturatedFat}g</h4>
-        <h4>Trans Fat: ${transFat}g</4>
+        <h3>Saturated Fat: ${saturatedFat}g</h3>
+        <h3>Trans Fat: ${transFat}g</h3>
         <h3>Cholesterol: ${cholesterol}mg</h3>
         <h3>Sodium: ${sodium}mg</h3>
-        <h3>Total Carbohydrates: ${totalCarbs}</h3>
-        <h4>Dietary Fiber: ${fiber}</h4>
-        <h4>Sugar: ${sugar}g</h4>
+        <h3>Total Carbohydrates: ${totalCarbs}g</h3>
+        <h3>Dietary Fiber: ${fiber}g</h3>
+        <h3>Sugar: ${sugar}g</h3>
         <h3>Protein: ${protein}g</h3>
         <hr>`;
     document.querySelector("#nutritionLabel").appendChild(newDiv);
